6ccb3b7f5ddf183df6856708783a1b5ab2f51939
Damien Schneider <damien.schneider01@gmail.com>
2023-01-19 23:25:48 +0100

    :sparkles: Add new books into the database

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 
+// firebase imports
+import { db } from '../firebase/config'
+import { collection, addDoc } from 'firebase/firestore'
+
 export default function BookForm() {
   const [newBook, setNewBook] = useState('')
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(newBook)
+
+    const ref = collection(db, 'books')
+    
+    await addDoc(ref, {
+      title: newBook
+    })
 
     setNewBook('')
   }
