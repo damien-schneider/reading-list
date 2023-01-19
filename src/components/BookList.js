98c7f682a8c52b38a5decbc21dda9541d3a4ff70
Damien Schneider <damien.schneider01@gmail.com>
2023-01-19 23:56:08 +0100

    :sparkles: Add possibility to delete books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,30 @@
+import delete_fill0 from '../assets/icons/delete_FILL0.svg'
+import delete_fill1 from '../assets/icons/delete_FILL1.svg'
+
+import { db } from '../firebase/config'
+import { deleteDoc, doc } from 'firebase/firestore'
+
 export default function BookList({ books }) {
 
   const handleClick = async (id) => {
-    console.log(id)
+    const docRef = doc(db, 'books', id)
+    await deleteDoc(docRef)
   }
 
-  return (
+  return (    
     <div className="book-list">
       <ul>
         {books.map(book => (
-          <li key={book.id} onClick={() => handleClick(book.id)}>{book.title}</li>
+          <li key={book.id} >
+            {book.title}
+            <img 
+              onClick={() => handleClick(book.id)}
+              src={delete_fill0} 
+              onMouseEnter={e => e.currentTarget.src = delete_fill1}
+              onMouseLeave={e => e.currentTarget.src = delete_fill0}
+              style={{width: '20px', height: '20px', marginLeft: '10px', display: 'inline-block', float: 'right', cursor: 'pointer'}}
+            />
+          </li>
         ))}
       </ul>
     </div>
