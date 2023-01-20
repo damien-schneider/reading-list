c9ca40528505b43574a9ab66aa2cac945bcc3cfa
Damien Schneider <damien.schneider01@gmail.com>
2023-01-20 23:11:39 +0100

    Create login and implement it to the component

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+//firebase imports
+import { auth } from "../firebase/config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export const useLogin = () => {
+    const [error, setError] = useState(null);
+    const login = (email, password) => {
+        setError(null);
+        signInWithEmailAndPassword(auth, email, password)
+            .then((res) => {
+                console.log("user signed up: ", res.user);
+            })
+            .catch((err) => {
+                setError(err.message);
+            })
+        // login logic
+
+
+    }
+    return { error, login }
+}
